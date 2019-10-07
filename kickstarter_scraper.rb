97952b0bc43bd7a79e093a214344710404f3caff
Bernadette Davis <bsd231@nyu.edu>
2019-10-07 21:22:33 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,9 +9,11 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   projects = {}
- 
+
   # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
   end
+
+  projects
 end
