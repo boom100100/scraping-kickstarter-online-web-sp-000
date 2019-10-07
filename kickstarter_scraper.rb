dcc0224023f86d6b47b0472e9f47a905fd7d08b6
Bernadette Davis <bsd231@nyu.edu>
2019-10-07 21:22:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,16 +1,17 @@
 # require libraries/modules here
 require 'nokogiri'
-require "pry"
 
 
 def create_project_hash
   # write your code here
+
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
-  # projects: kickstarter.css("li.project.grid_4")
-  # title = project.css("h2.bbcard_name strong a").text
 
-
+  projects = {}
+ 
+  # Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
 end
-create_project_hash
